03afd0e0116640c52ea165bc600326fe26d68406
NyAndriaX <tsilavinaandriamahafaly01@gmail.com>
2024-07-06 10:20:00 +0300

    feat:add a icon in a project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-const Navbar:React.FC = () => {
-  return (
-    <div>Navbar</div>
-  )
+interface NavbarProps {
+  title: string;
 }
-export default Navbar
+
+const Navbar: React.FC<NavbarProps> = ({ title }) => {
+  return (
+    <div className='navbar bg-base-100'>
+      <div className='flex-1'>
+        <a className='btn btn-ghost text-xl'>{title}</a>
+      </div>
+      <div className='flex-none'>
+        <ul className='menu menu-horizontal px-1'>
+          <li>
+            <a>Link</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className='bg-base-100 rounded-t-none p-2'>
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
